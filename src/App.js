99b177c452a23da9811a419708f3fe35a05317dd
Shimaahamdi2003 <Shimaahamdi2003@gmail.com>
2024-04-30 04:47:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React,{useEffect, useState} from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import { MagicSpinner  } from "react-spinners-kit";
+import "./App.css" 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './component/Home'
+import NavBar from './component/NavBar'
+import Projects from './component/Projects';
+
+const App = () => {
+
+    const [loading , setLoading]= useState(false);
+    
+      useEffect(()=>{
+          setLoading(true);
+
+          setTimeout(()=>{
+              setLoading(false)
+          },3000)
+
+
+      }, [])
+
+
+  return (
+    <BrowserRouter>
+
+        <div>
+        <NavBar/>
+        </div>
+
+        {loading ?
+        <div className='preloaderr'>
+         <MagicSpinner  size={200} color="#fff"/>
+
+          </div>
+          :
+          <>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/Projects' element={<Projects />}/>
+            
+          </Routes></>
+      }
+
+        
+      
+    
+
+      
+
+    </BrowserRouter>
+  )
+}
+
+export default App
